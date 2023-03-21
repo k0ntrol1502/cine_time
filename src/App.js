1f2951ab2e66401ebb78d10dcc904165ab516190
k0ntrol1502 <92878832+k0ntrol1502@users.noreply.github.com>
2023-03-21 23:03:13 +0530

    Finished  Header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App" id="outer-container">
-    <Box bg='tomato' w='100%' p={6} color='white'>
+    <Box bg='tomato' w='100%' p={7} color='white' fontSize={25} boxShadow="5px 3px #888888">
       Cine Time
     </Box>
       <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
