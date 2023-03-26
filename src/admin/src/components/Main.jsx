c3ca87c8fc808458ef43b89736107d9f13a4a6b1
k0ntrol1502 <92878832+k0ntrol1502@users.noreply.github.com>
2023-03-26 18:24:44 +0530

    Trying to add an admin panel

---
+++ src/admin/src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './css/TopHeader.css'; 
+const Main = () => {
+  return (
+    <>
+        <div classname="container-fluid">
+            <div classname="row">
+                <div classname="col-12 top_header">
+                
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+export default Main;
