56ca81cb30cc8620822d6ecff82eb989c68493bd
k0ntrol1502 <92878832+k0ntrol1502@users.noreply.github.com>
2023-04-06 20:28:10 +0530

    changing the movie posters

--- src/admin/src/components/Main.jsx
+++ src/admin/src/components/Main.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './css/TopHeader.css'; 
+
 const Main = () => {
   return (
     <>
         <div classname="container-fluid">
-            <div classname="row">
-                <div classname="col-12 top_header">
-                
+            <div className="row">
+                <div className="col-12 top_header">
+
                 </div>
             </div>
         </div>
     </>
-  )
+  );
 }
 export default Main;
